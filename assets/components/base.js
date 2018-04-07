666a602db29d5d2fd56094a84bb363bf8a3477ed
Julien Minguely <jminguely@gmail.com>
2018-04-07 14:55:40 +0200

    Adds the agenda page in the styleguide

--- assets/components/base.js
+++ assets/components/base.js
@@ -1,9 +1,11 @@
 /* global jQuery */
 
 import navigation from './organisms/navigation/navigation';
+import layout from './layout';
 
 (($) => {
   $(document).ready(() => {
+    layout();
     navigation();
   });
 })(jQuery);
