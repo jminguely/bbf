8a814d0a5b03841043af18625cfa1891e5fe4c6d
Julien Minguely <jminguely@gmail.com>
2018-06-06 19:02:06 +0200

    Makes the logo partner more responsive

--- web/app/themes/bbf/app/src/Config/Images.php
+++ web/app/themes/bbf/app/src/Config/Images.php
@@ -6,7 +6,8 @@ class Images {
     public static function register() {
         add_action( 'after_setup_theme', function () {
             add_image_size('gallery-thumbnail', 700, 440, true);
-            add_image_size('logo', 400, 200);
+            add_image_size('logo', 200, 100);
+            add_image_size('logo_2x', 400, 200);
             add_image_size('cover', 2000);
         });
     }
